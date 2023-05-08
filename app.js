ed989463f3557ff0c26d75517519a2b8d86d749a
Alexander Brown <72706105+Saladalec@users.noreply.github.com>
2023-05-08 15:45:15 -0400

    Functionality and design
    Added a reset game button, added a tally for the total number of guesses, and made some overall design changes

--- app.js
+++ app.js
@@ -65,60 +65,77 @@ const cardArray = [
     },
 ]
 
-cardArray.sort(() => 0.5 - Math.random());
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", resetGame);
 const grid = document.querySelector('#grid');
 const score = document.querySelector('#score');
-score.textContent = '0';
-let chosenCards =[];
-let chosenCardsIds = [];
-const cardsWon = [];
+const guesses = document.querySelector('#guesses');
 
-function createBoard() {
-    for(let i = 0; i < cardArray.length; i++){
-        const card = document.createElement('img');
-        card.setAttribute('src', 'images/back.png');
-        card.setAttribute('data-id', i);
-        card.addEventListener('click', flipCard);
-        grid.append(card);
+playGame();
+
+function resetGame(){
+    for(let i = 0; i<16; i++){
+        grid.removeChild(grid.lastChild);
     }
+    playGame();
 }
 
-createBoard();
+function playGame(){
+    score.textContent = '0';
+    guesses.textContent = '0';
+    let numberGuesses = 0;
+    let chosenCards =[];
+    let chosenCardsIds = [];
+    let cardsWon = [];
 
-function checkMatch(){
-    const cards = document.querySelectorAll('img');
+    cardArray.sort(() => 0.5 - Math.random());
+    
+    function createBoard() {
+        for(let i = 0; i < cardArray.length; i++){
+            const card = document.createElement('img');
+            card.setAttribute('src', 'images/back.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.append(card);
+        }
+    }
 
-    if (chosenCardsIds[0] == chosenCardsIds[1]){
-        alert('You clicked the same card!');
-        cards[chosenCardsIds[0]].setAttribute('src', 'images/back.png');
-        cards[chosenCardsIds[1]].setAttribute('src', 'images/back.png');
-    }
-    else if(chosenCards[0] == chosenCards[1]){
-        cards[chosenCardsIds[0]].setAttribute('src', 'images/Green.png');
-        cards[chosenCardsIds[1]].setAttribute('src', 'images/Green.png');
-        cards[chosenCardsIds[0]].removeEventListener('click', flipCard);
-        cards[chosenCardsIds[1]].removeEventListener('click', flipCard);
-        cardsWon.push(chosenCards);
-    }
-    else {
-        cards[chosenCardsIds[0]].setAttribute('src', 'images/back.png');
-        cards[chosenCardsIds[1]].setAttribute('src', 'images/back.png');
-    }
-    score.textContent = cardsWon.length;
-    chosenCards = [];
-    chosenCardsIds = [];
+    createBoard();
 
-    if(cardsWon.length == 8){
-        score.textContent = "You won!";
+    function checkMatch(){
+        const cards = document.querySelectorAll('img');
+        if (chosenCardsIds[0] == chosenCardsIds[1]){
+            alert('You clicked the same card!');
+            cards[chosenCardsIds[0]].setAttribute('src', 'images/back.png');
+            cards[chosenCardsIds[1]].setAttribute('src', 'images/back.png');
+        }
+        else if(chosenCards[0] == chosenCards[1]){
+            cards[chosenCardsIds[0]].setAttribute('src', 'images/Green.png');
+            cards[chosenCardsIds[1]].setAttribute('src', 'images/Green.png');
+            cards[chosenCardsIds[0]].removeEventListener('click', flipCard);
+            cards[chosenCardsIds[1]].removeEventListener('click', flipCard);
+            cardsWon.push(chosenCards);
+            numberGuesses++;
+            guesses.textContent = numberGuesses;
+        }
+        else {
+            cards[chosenCardsIds[0]].setAttribute('src', 'images/back.png');
+            cards[chosenCardsIds[1]].setAttribute('src', 'images/back.png');
+            numberGuesses++;
+            guesses.textContent = numberGuesses;
+        }
+        score.textContent = cardsWon.length;
+        chosenCards = [];
+        chosenCardsIds = [];
     }
-}
 
-function flipCard(){
-    const cardID = this.getAttribute('data-id');
-    chosenCards.push(cardArray[cardID].name);
-    chosenCardsIds.push(cardID);
-    this.setAttribute('src', cardArray[cardID].img);
-    if (chosenCards.length === 2){
-        setTimeout(checkMatch, 500);
+    function flipCard(){
+        const cardID = this.getAttribute('data-id');
+        chosenCards.push(cardArray[cardID].name);
+        chosenCardsIds.push(cardID);
+        this.setAttribute('src', cardArray[cardID].img);
+        if (chosenCards.length === 2){
+            setTimeout(checkMatch, 500);
+        }
     }
 }
